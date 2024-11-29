df68277faa9db8c3545cbe7155eba013a992987e
Sanjay Khatri <sanjay.kumar@arittek.com>
2024-11-29 13:13:46 +0500

    First Commit

---
+++ O2ChatSDK.xcframework/ios-arm64_x86_64-simulator/O2ChatSDK.framework/Headers/O2ChatSDK.h
@@ -0,0 +1,18 @@
+//
+//  O2ChatSDK.h
+//  O2ChatSDK
+//
+//  Created by Sanjay Kumar on 12/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for O2ChatSDK.
+FOUNDATION_EXPORT double O2ChatSDKVersionNumber;
+
+//! Project version string for O2ChatSDK.
+FOUNDATION_EXPORT const unsigned char O2ChatSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <O2ChatSDK/PublicHeader.h>
+
+
